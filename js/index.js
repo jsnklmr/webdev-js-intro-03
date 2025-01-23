699e811adcf8eb0ca8ec2eec357297d15350ba9d
Jason Kilmer <jkilmer@thedixongroup.com>
2025-01-23 10:54:57 -0500

    task part 2 complete

--- js/index.js
+++ js/index.js
@@ -2,4 +2,25 @@
 // that are not declared
 "use strict"
 
-console.log(1+1);
+/*console.log(1+1);
+console.log(1 + 1);        
+console.log(1 - 1);         
+console.log(1 + "1");        
+                            
+
+console.log(3 - 4 * 5)       
+console.log((3 - 4) * 5);    
+                            
+
+console.log(6 / 5);          
+console.log(6 % 5);          
+                            
+
+console.log(1 == 1);         
+console.log(1 == "1");      
+console.log(1 === "1");      
+
+console.log(1 || 2);         
+console.log(2 || 1);        
+console.log(1 && 2);         
+console.log(1 && 1); */
